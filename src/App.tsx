3e76e41423f0b90c72ab306979ef94d97dc9b8ab
Andres Bedoya <andresbedoyag@Andress-MacBook-Pro.local>
2024-01-05 19:28:14 -0500

    Add platejs editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import Layout from "@/components/layout"
+import Editor from "@/components/editor"
 
 function App() {
   return (
     <Layout>
-      <main className="m-3 flex flex-1 gap-4 rounded-lg border bg-white p-4 md:gap-8 md:p-6">
-        <h1>Holi</h1>
+      <main className="m-3 flex flex-1 flex-col gap-4 rounded-lg border bg-white p-4 md:gap-8 md:p-6">
+        <Editor />
       </main>
     </Layout>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import { TooltipProvider } from "@/components/plate-ui/tooltip"
+
 import App from "./App"
 
 import "./styles.css"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <TooltipProvider disableHoverableContent delayDuration={500} skipDelayDuration={0}>
+      <App />
+    </TooltipProvider>
   </React.StrictMode>
 )
